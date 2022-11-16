a7b2e199ffd9170eded8f28057c43137f4dc4e22
keskami <kkak808@gmail.com>
2022-11-16 09:58:28 -1000

    modified deployment target

--- designcode_app/ios/designcodeapp/AppDelegate.h
+++ designcode_app/ios/designcodeapp/AppDelegate.h
@@ -1,7 +1,6 @@
 #import <Foundation/Foundation.h>
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
-
 #import <Expo/Expo.h>
 
 @interface AppDelegate : EXAppDelegateWrapper <RCTBridgeDelegate>
