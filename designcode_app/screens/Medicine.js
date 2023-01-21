d387442cd14bde7cf78978f0eb5d8a403b8f8a56
Kestan Kamei <kk@Kestans-MacBook-Pro.local>
2023-01-21 09:19:27 -1000

    medicine api

--- designcode_app/screens/Medicine.js
+++ designcode_app/screens/Medicine.js
@@ -1,47 +1,138 @@
-import React, { useEffect } from "react";
-import styled from "styled-components";
+import React, { useState, useEffect } from "react";
 import {
-  StyleSheet,
-  Pressable,
-  ScrollView,
-  SafeAreaView,
+  View,
+  TextInput,
   Text,
-  Button,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
 } from "react-native";
+import axios from "axios";
+import {Picker} from '@react-native-picker/picker';
 
-
-
-class MedicineScreen extends React.Component {
-  static navigationOptions = {
-    headerShown: false,
-  };
-
-  componentDidMount() {
-    return fetch("../assets/drugbank-ui.min.js")
-    .then((response) => response.json())
-    .then((responseJson) => {
-      
-    })
-    
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    flexDirection: "column",
+    alignItems: "stretch",
+    justifyContent: "center"
+  },
+  searchBar: {
+    borderWidth: 1,
+    borderColor: "lightblue",
+    borderRadius: 10,
+    padding: 10,
+    fontSize: 16
+  },
+  selectedMedicine: {
+    fontSize: 20,
+    padding: 10,
+    alignSelf: "center",
+    textAlign: "center"
+  },
+  pickerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  picker: {
+    flex: 1
+  },
+  submitButton: {
+    padding: 10,
+    alignSelf: "center",
+    backgroundColor: "lightblue",
+    borderRadius: 5,
+    marginTop: 10
   }
+});
 
-  render() {
-    return (
-      <SafeAreaView>
-        <Container>
-          <Button
-            title="Medicine Intake Screen"
-            onPress={() => {
-              this.props.navigation.navigate("Section");
-            }}
-          />
-          
-        </Container>
-      </SafeAreaView>
+const MedicineScreen = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [medicines, setMedicines] = useState([]);
+  const [selectedMedicine, setSelectedMedicine] = useState("");
+  const [amountConsumed, setAmountConsumed] = useState("");
+  const [units, setUnits] = useState("");
+
+  useEffect(() => {
+    const handleSearch = async () => {
+      try {
+        const response = await axios.get(
+          `https://rxnav.nlm.nih.gov/REST/approximateTerm.json?term=${searchTerm}`
+        );
+        setMedicines(response.data.approximateGroup.candidate);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (searchTerm) {
+      handleSearch();
+    }
+  }, [searchTerm]);
+
+  useEffect(() => {
+    setMedicines(
+      medicines.filter((medicine) => medicine.name !== selectedMedicine)
     );
-  }
-}
+  }, [selectedMedicine]);
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.searchBar}
+        value={searchTerm}
+        onChangeText={setSearchTerm}
+        placeholder="Search for a medicine"
+      />
+      <FlatList
+        data={medicines}
+        keyExtractor={(item) => item.rxcui}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => setSelectedMedicine(item.name)}>
+            <Text>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
+      {!!selectedMedicine && (
+        <Text style={styles.selectedMedicine}>
+          Selected Medicine: {selectedMedicine}
+        </Text>
+      )}
+      <View style={styles.pickerContainer}>
+        <Picker
+          style={styles.picker}
+          selectedValue={amountConsumed}
+          onValueChange={(itemValue) => setAmountConsumed(itemValue)}
+        >
+          <Picker.Item label="Amount Consumed" value="" />
+          <Picker.Item label="1" value="1" />
+          <Picker.Item label="2" value="2" />
+          <Picker.Item label="3" value="3" />
+          <Picker.Item label="4" value="4" />
+          <Picker.Item label="5" value="5" />
+        </Picker>
+        <Picker
+          style={styles.picker}
+          selectedValue={units}
+          onValueChange={(itemValue) => setUnits(itemValue)}
+        >
+          <Picker.Item label="Units" value="" />
+          <Picker.Item label="mg" value="mg" />
+          <Picker.Item label="g" value="g" />
+          <Picker.Item label="ml" value="ml" />
+          <Picker.Item label="l" value="l" />
+        </Picker>
+      </View>
+      <TouchableOpacity
+        style={styles.submitButton}
+        onPress={() =>
+          console.log(`${selectedMedicine}: ${amountConsumed} ${units}`)
+        }
+      >
+        <Text>Submit</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default MedicineScreen;
-
-const Container = styled.View``;
