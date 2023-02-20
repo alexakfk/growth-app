be1a7aaf4a9e642f927ad4965da2d55fd907d43e
Kestan Kamei <kk@Kestans-Macbook-Pro.local>
2023-02-20 09:50:30 -1000

    health biometric data work

--- designcode_app/metro.config.js
+++ designcode_app/metro.config.js
@@ -1,3 +1,6 @@
+require('ts-node/register');
+module.exports = require('./metro.config.ts');
+
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("@expo/metro-config");
 
